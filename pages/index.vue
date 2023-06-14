3e7f9674d03528ebc58a4b1c9594b652dee384c5
Jon Snow <47143928+therealJonSnow@users.noreply.github.com>
2023-06-14 15:28:29 +0100

    add test form

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,34 @@
     HOME
     <NuxtLink to="/blog">Go To Blog</NuxtLink>
   </div>
+  <div class="mt-8">
+    <form
+      name="contact"
+      method="POST"
+      netlify-honeypot="bot-field"
+      data-netlify="true"
+    >
+      <p class="hidden">
+        <label>
+          Don’t fill this out if you’re human: <input name="bot-field" />
+        </label>
+        <input type="hidden" name="subject" value="Reservation Request" />
+      </p>
+      <p>
+        <label>
+          Email: <input type="text" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message"></textarea>
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  </div>
 </template>
 
 <script setup>
